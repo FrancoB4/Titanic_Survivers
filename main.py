138673d5175586565a20d9f6b8a4f560fdd0db13
FrancoB4 <francobonfigliovazquez@gmail.com>
2021-12-21 16:24:17 -0300

    Score 0.77511

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from datetime import datetime
-from pandas import DataFrame
-from auxiliar_functions import one_preprocessing, read_csv
+import pandas as pd
+from auxiliar_functions import one_preprocessing
 from sklearn.ensemble import RandomForestClassifier
 
 
 def run():
-    train_data, test_data = read_csv('./data/train.csv'), read_csv('./data/test.csv')
+    train_data, test_data = pd.read_csv(
+        './data/train.csv'), pd.read_csv('./data/test.csv')
 
     x_train, y_train = one_preprocessing(train_data)
     x_test = one_preprocessing(test_data)
 
-    model = RandomForestClassifier(n_estimators=150, max_depth=6).fit(x_train, y_train)
+    model = RandomForestClassifier(
+        n_estimators=150, max_depth=6).fit(x_train, y_train)
 
     predictions = model.predict(x_test)
 
-    predictions_df = DataFrame(predictions, index=test_data['PassengerId'],
-                                    columns=['Survived'])
+    predictions_df = pd.DataFrame(predictions, index=test_data['PassengerId'],
+                                  columns=['Survived'])
 
-    predictions_df.to_csv(f'./results/{datetime.now()}.csv')
+    predictions_df.to_csv(
+        f'./results/{datetime.now().day}_{datetime.now().month}_{datetime.now().year}.csv')
+
 
 if __name__ == '__main__':
     run()
